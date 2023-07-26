a01f4b4a5f32c0ce994022afcd9477df3e00e1f3
xj <1060077601@qq.com>
2023-07-26 16:58:25 +0800

    Add user-defined parameters

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # jwt
 A simple library of JWT
+
+## Installation
+
+To install jwt run the command:
+
+    composer require worldxyj/jwt
+
+
+## Quick start
+
+```php
+use Worldxyj\Jwt;
+
+$jwt = new Jwt('your own secret');
+
+$payload = [
+    'name' => 'worldxyj/jwt',
+    'des' => '一个简单的JWT库'
+];
+
+$reponseDefault = $jwt->getToken($payload);// 默认过期时间1天
+
+$reponseExp = $jwt->getTokenWithExp($payload, 3600 * 24);// 自定义过期时间
+
+$res = $jwt->verifyToken($reponseDefault['data']);// 验证token
+
+if($res['errcode'] == 0){
+    $payload = $res['data'];
+    echo '验证通过';
+}
+```
--- src/Jwt.php
+++ src/Jwt.php
@@ -6,7 +6,6 @@ class Jwt
 {
     /**
      * 头部
-     *
      * @var string[]
      */
     private static $header = [
@@ -17,43 +16,61 @@ class Jwt
     /**
      * 使用 HMAC 生成信息摘要时所使用的密钥
      * 生产环境请修改秘钥
-     *
      * @var string
      */
-    private static $key = 'secret';
+    public $secret;
+
+    /**
+     * 义在什么时间之前，该jwt都是不可用的
+     * @var int
+     */
+    public $nbf;
+
+    /**
+     * jwt签发者
+     * @var string
+     */
+    public $iss;
+
+    /**
+     * jwt所面向的用户
+     * @var string
+     */
+    public $sub;
+
+    public function __construct($secret = 'secret', $nbf = 0, $iss = '', $sub = '' )
+    {
+        $this->secret = $secret;
+        $this->nbf = $nbf;
+        $this->iss = $iss;
+        $this->sub = $sub;
+    }
 
     /**
      * 获取jwt token
+     * 默认过期时间1天
      *
-     * @param array $payload jwt载荷  格式如下非必须
-     * [
-     *   'iss'=>'jwt_admin',              //该JWT的签发者
-     *   'iat'=>time(),                   //签发时间
-     *   'exp'=>time()+7200,              //过期时间
-     *   'nbf'=>time()+60,                //该时间之前不接收处理该Token
-     *   'sub'=>'www.admin.com',          //面向的用户
-     *   'jti'=>md5(uniqid('JWT').time()) //该Token唯一标识
-     * ]
-     * @return bool|string
+     * @param array $payload jwt载荷
+     * @return array
      */
-    public static function getToken(array $payload)
+    public function getToken($payload)
     {
         $arr = [
-            'iss' => 'chiyingkeji', //该JWT的签发者
+            'iss' => $this->iss, //该JWT的签发者
             'iat' => time(), //签发时间
             'exp' => time() + 3600 * 24, //过期时间
-            'nbf' => time(), //该时间之前不接收处理该Token
-            'sub' => '', //面向的用户
+            'nbf' => $this->nbf, //定义在什么时间之前，该jwt都是不可用的
+            'sub' => $this->sub, //面向的用户
             'jti' => md5(uniqid('JWT') . time()) //该Token唯一标识
         ];
         $payload = array_merge($arr, $payload);
         if (is_array($payload)) {
             $base64header = self::base64UrlEncode(json_encode(self::$header, JSON_UNESCAPED_UNICODE));
             $base64payload = self::base64UrlEncode(json_encode($payload, JSON_UNESCAPED_UNICODE));
-            $token = $base64header . '.' . $base64payload . '.' . self::signature($base64header . '.' . $base64payload, self::$key, self::$header['alg']);
-            return $token;
+            $token = $base64header . '.' . $base64payload . '.' . $this->signature($base64header . '.' . $base64payload, $this->secret, self::$header['alg']);
+            return $this->responseSuccess($token);
         } else {
-            throw new \Exception('payload格式错误');
+            return $this->responseError(106, 'payload格式错误');
         }
     }
 
@@ -63,28 +80,27 @@ class Jwt
      *
      * @param array $payload
      * @param int $exp
-     * @return string
-     * @throws \Exception
+     * @return array
      *
      */
-    public static function getTokenWithExp(array $payload, int $exp)
+    public function getTokenWithExp($payload, $exp)
     {
         $arr = [
-            'iss' => 'chiyingkeji', //该JWT的签发者
+            'iss' => $this->iss, //该JWT的签发者
             'iat' => time(), //签发时间
-            'exp' => $exp, //过期时间
-            'nbf' => time(), //该时间之前不接收处理该Token
-            'sub' => '', //面向的用户
+            'exp' => time() + (int)$exp, //过期时间
+            'nbf' => $this->nbf, //该时间之前不接收处理该Token
+            'sub' => $this->sub, //面向的用户
             'jti' => md5(uniqid('JWT') . time()) //该Token唯一标识
         ];
         $payload = array_merge($arr, $payload);
         if (is_array($payload)) {
             $base64header = self::base64UrlEncode(json_encode(self::$header, JSON_UNESCAPED_UNICODE));
             $base64payload = self::base64UrlEncode(json_encode($payload, JSON_UNESCAPED_UNICODE));
-            $token = $base64header . '.' . $base64payload . '.' . self::signature($base64header . '.' . $base64payload, self::$key, self::$header['alg']);
-            return $token;
+            $token = $base64header . '.' . $base64payload . '.' . $this->signature($base64header . '.' . $base64payload, $this->secret, self::$header['alg']);
+            return $this->responseSuccess($token);
         } else {
-            throw new \Exception('payload格式错误');
+            return $this->responseError(106, 'payload格式错误');
         }
     }
 
@@ -92,13 +108,13 @@ class Jwt
      * 验证token是否有效,默认验证exp,nbf,iat时间
      *
      * @param string $Token 需要验证的token
-     * @return bool|string
+     * @return array
      */
-    public static function verifyToken(string $Token)
+    public function verifyToken($Token)
     {
         $tokens = explode('.', $Token);
         if (count($tokens) != 3){
-            throw new \Exception('token格式错误');
+            return $this->responseError(101, 'token格式错误');
         }
 
         list($base64header, $base64payload, $sign) = $tokens;
@@ -106,32 +122,32 @@ class Jwt
         //获取jwt算法
         $base64decodeheader = json_decode(self::base64UrlDecode($base64header), JSON_OBJECT_AS_ARRAY);
         if (empty($base64decodeheader['alg'])){
-            throw new \Exception('alg错误');
+            return $this->responseError(102, 'alg错误');
         }
 
         //签名验证
-        if (self::signature($base64header . '.' . $base64payload, self::$key, $base64decodeheader['alg']) !== $sign){
-            throw new \Exception('签名验证失败');
+        if ($this->signature($base64header . '.' . $base64payload, $this->secret, $base64decodeheader['alg']) !== $sign){
+            return $this->responseError(100, '签名验证失败');
         }
 
         $payload = json_decode(self::base64UrlDecode($base64payload), JSON_OBJECT_AS_ARRAY);
 
         //签发时间大于当前服务器时间验证失败
         if (isset($payload['iat']) && $payload['iat'] > time()){
-            throw new \Exception('签发时间大于当前服务器时间');
+            return $this->responseError(103, '签发时间大于当前服务器时间');
         }
 
         //过期时间小宇当前服务器时间验证失败
         if (isset($payload['exp']) && $payload['exp'] < time()){
-            throw new \Exception('token已过期');
+            return $this->responseError(104, 'token已过期');
         }
 
-        //该nbf时间之前不接收处理该Token
-//        if (isset($payload['nbf']) && $payload['nbf'] > time()){
-//            throw new \Exception('时间错误');
-//        }
+        //定义在什么时间之前，该jwt都是不可用的
+        if (isset($payload['nbf']) && $payload['nbf'] > time()){
+            return $this->responseError(105, '未到生效时间');
+        }
 
-        return $payload;
+        return $this->responseSuccess($payload);
     }
 
     /**
@@ -141,7 +157,7 @@ class Jwt
      * @param string $input 需要编码的字符串
      * @return string
      */
-    private static function base64UrlEncode(string $input)
+    private static function base64UrlEncode($input)
     {
         return str_replace('=', '', strtr(base64_encode($input), '+/', '-_'));
     }
@@ -153,7 +169,7 @@ class Jwt
      * @param string $input 需要解码的字符串
      * @return bool|string
      */
-    private static function base64UrlDecode(string $input)
+    private static function base64UrlDecode($input)
     {
         $remainder = strlen($input) % 4;
         if ($remainder) {
@@ -172,11 +188,40 @@ class Jwt
      * @param string $alg 算法方式
      * @return mixed
      */
-    private static function signature(string $input, string $key, string $alg = 'HS256')
+    private function signature($input)
     {
-        $alg_config = array(
-            'HS256' => 'sha256'
-        );
-        return self::base64UrlEncode(hash_hmac($alg_config[$alg], $input, $key, true));
+        $hash = hash_hmac('sha256', $input, $this->secret, true);
+        return self::base64UrlEncode($hash);
+    }
+
+    /**
+     * 验证成功
+     *
+     * @param $playload
+     * @return array
+     */
+    protected function responseSuccess($playload)
+    {
+        return [
+            'errcode' => 0,
+            'msg' => '',
+            'data' => $playload,
+        ];
+    }
+
+    /**
+     * 验证失败
+     *
+     * @param $errcode
+     * @param $msg
+     * @return array
+     */
+    protected function responseError($errcode, $msg)
+    {
+        return [
+            'errcode' => $errcode,
+            'msg' => $msg,
+            'data' => [],
+        ];
     }
 }
